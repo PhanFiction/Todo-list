3803b5616fdb9002a9ffd45ef351ad8fa07d1008
Steve <stevewu24@gmail.com>
2023-10-12 13:37:13 -0700

    refactored code for Task and Project component

--- frontend/src/components/Task/Task.jsx
+++ frontend/src/components/Task/Task.jsx
@@ -1,43 +1,52 @@
-import styles from './Task.module.css';
+import React from 'react';
 import CheckBox from '../CheckBox/CheckBox';
 import FaIcon from '../FaIcon/FaIcon';
 import Button from '../Button/Button';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
 import LinkRoute from '../LinkRoute/LinkRoute';
+import styles from './Task.module.css';
 
-const Task = ({ completed, taskId, setTaskData, taskData, children }) => {
-  const [complete, isComplete] = useState(completed);
-  const randomColors = ['border-l-orange', 'border-l-green', 'border-l-red', 'border-l-blue'];
+const Task = ({
+  completed,
+  taskId,
+  toggleComplete,
+  handleDelete,
+  dueDate,
+  children,
+}) => {
+  const randomColors = [
+    styles['border-l-orange'],
+    styles['border-l-green'],
+    styles['border-l-red'],
+    styles['border-l-blue'],
+  ];
 
-  const toggleComplete = (taskId) => {
-    isComplete(!complete);
-  };
+  const date = new Date(dueDate);
+  const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+  const formattedDate = date.toLocaleDateString(undefined, options);
 
-  const handleDelete = (taskId) => {
-    const deletedTask = taskData.filter(item => item.id !== taskId);
-    setTaskData(deletedTask);
-  };
-
-  return(
+  return (
     <div
       className={`${styles['task-container']} ${
         randomColors[Math.floor(Math.random() * 4)]
       }`}
     >
-      <CheckBox handleCompleted={() => toggleComplete(taskId)} completed={complete}>
+      <CheckBox handleCompleted={() => toggleComplete(taskId)} completed={completed}>
         {children}
       </CheckBox>
       <div className={styles['task-items']}>
+        <div>
+          <p>{formattedDate}</p>
+        </div>
         <LinkRoute to={`/task/${taskId}`}>
           <Button noBorder={false}>Details</Button>
         </LinkRoute>
         <Button handleClick={() => handleDelete(taskId)}>
-          <FaIcon iconName={faTrashCan} size="sm" onClick={handleDelete} />
+          <FaIcon iconName={faTrashCan} size='sm' onClick={handleDelete} />
         </Button>
       </div>
     </div>
-  )
+  );
 };
 
 export default Task;
--- frontend/src/pages/Project/Project.jsx
+++ frontend/src/pages/Project/Project.jsx
@@ -10,27 +10,50 @@ const projectService = require('../../services/project');
 
 const Project = () => {
   const [project, setProjects] = useState([]);
+  const [complete, isComplete] = useState('');
   const projectId = useParams().id;
 
   useEffect(() => {
     projectService.getProject(projectId).then(data => setProjects(data));
-    projectService.getProject(projectId).then(data => console.log(data));
   }, [projectId]);
+
+  const toggleComplete = (taskId) => {
+    // Create a copy of the project's tasks
+    const updatedTasks = project.tasks.map(task => {
+      if (task._id === taskId) {
+        // Toggle the 'completed' property for the specific task
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+
+    // Update the project state with the updated tasks
+    setProjects({ ...project, tasks: updatedTasks });
+  };
+
+  const handleDelete = (taskId) => {
+    const deletedTasks = project.tasks.filter(task => task._id !== taskId);
+    setProjects({ ...project, tasks: deletedTasks });
+  };
 
   return(
     <Page>
       <div className={styles['project-section']}>
         <h1>Project: { project.title }</h1>
         <div>
-          {project.tasks === undefined || project.tasks.length === 0 ? <h2>Project is empty</h2>
+          {project.tasks === undefined || project.tasks.length === 0 ? <h3>Project is empty</h3>
             : project.tasks.map((item, index) =>
               <Task completed={item.completed}
                 key={index}
-                taskId={item.id}
+                taskId={item._id}
                 taskData={project}
                 setTaskData={setProjects}
+                dueDate={item.dueDate}
+                toggleComplete={toggleComplete}
+                handleDelete={handleDelete}
+                complete={complete}
               >
-                {item.name}
+                {item.title}
               </Task>
             )
           }
