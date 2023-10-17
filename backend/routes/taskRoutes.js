01457911ad36f6cfbe0fcc3669a276cf8f58ae27
Steve <stevewu24@gmail.com>
2023-10-17 11:11:22 -0700

    fixed the code to display the correct route

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const taskController = require('../controller/taskController');
 
 router.post('/create-task', taskController.createTask);
+router.get('/today-task', taskController.getTodayTasks);
+router.get('/weekly-task', taskController.getWeekTasks);
 router.get('/', taskController.getAllTasks);
 router.get('/:id', taskController.getSingleTask);
 router.put('/:id', taskController.updateTask);
