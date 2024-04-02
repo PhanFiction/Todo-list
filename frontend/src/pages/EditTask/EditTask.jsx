d96a237044d8df8bdb2843e740c32e5fde86a8d6
Steve <stevewu24@gmail.com>
2024-04-02 03:39:47 -0700

    updated EditTask added useEffect, handleSubmit, try catch error

--- frontend/src/pages/EditTask/EditTask.jsx
+++ frontend/src/pages/EditTask/EditTask.jsx
@@ -1,50 +1,69 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './EditTask.module.css';
 import Page from '../../components/Page/Page';
 import TaskForm from '../../components/TaskForm/TaskForm';
+import { useParams } from 'react-router-dom';
+import { useFetchProjects } from '../../hooks/useFetchData';
 
-const EditTask = () => {
+const taskService = require('../../services/task');
+
+const EditTask = ({ setAlert }) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [priority, setPriority] = useState('');
   const [project, setProject] = useState('Inbox');
   const [dueDate, setDueDate] = useState('');
+  const [task, setTask] = useState({});
+  const { projects } = useFetchProjects();
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  }
+  const handleTitleChange = (e) => setTitle(e.target.value);
+  const handleDescriptionChange = (e) => setDescription(e.target.value);
+  const handlePriorityChange = (e) => setPriority(e.target.value);
+  const handleProjectChange = (e) => setProject(e.target.value);
+  const handleDueDateChange = (e) => setDueDate(e.target.value);
 
-  const handleDescriptionChange = (e) => {
-    setDescription(e.target.value);
-  }
+  const taskId = useParams().id;
 
-  const handlePriorityChange = (e) => {
-    setPriority(e.target.value);
-  };
+  useEffect(() => {
+    const fetchTaskData = async () => {
+      const fetchedTask = await taskService.getTask(taskId);
+      const date = new Date(fetchedTask.dueDate);
+      const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+      const formattedDate = date.toLocaleDateString(undefined, options);
+      setTask({...fetchedTask, dueDate: formattedDate});
+      console.log(fetchedTask);
+    };
 
-  const handleProjectChange = (e) => {
-    setProject(e.target.value);
-  }
+    fetchTaskData();
+  },[]);
 
-  const handleDueDateChange = (e) => {
-    setDueDate(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('due date', dueDate, ' project ', project, ' title ', title, ' priotity ', priority, ' description ', description);
-  }
+    const newTask = {
+      title,
+      description,
+      priority,
+      dueDate,
+      projectId: project,
+    }
 
+    try {
+      const res = await taskService.updateTask(taskId, newTask);
+      if(res.success) setAlert({'message': res.success});
+    } catch(error) {
+      console.log('failed to updated task ', error);
+    }
+  }
 
   return(
     <Page>
       <div className={styles["edit-task-page"]}>
         <div className={styles["edit-task-container"]}>
           <TaskForm
-            title={title}
-            description={description}
-            priority={priority}
-            project={project}
+            title={task?.title}
+            description={task?.description}
+            priority={task?.priority}
+            projects={projects}
             dueDate={dueDate}
             titleChange={handleTitleChange}
             descriptionChange={handleDescriptionChange}
