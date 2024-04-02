df4a36bfd35671e82b39980ca193dc9ba4a502b3
Steve <stevewu24@gmail.com>
2024-04-02 08:23:34 -0700

    updated Home page component and css

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,82 +1,40 @@
-import styles from './Home.module.css';
+import './Home.css';
 import Task from '../../components/Task/Task';
 import Page from '../../components/Page/Page';
 import { useState, useEffect } from 'react';
-import {
-  Redirect,
-} from "react-router-dom";
+const taskService = require('../../services/task');
 
-const fakeApi = [
-  {
-    name: 'Cook Pasta',
-    completed: false,
-    id: 1,
-  },
-  {
-    name: 'Get Groceries',
-    completed: true,
-    id: 2,
-  },
-  {
-    name: 'Get Pasta ingredients and chocolates from safeway',
-    completed: false,
-    id: 3,
-  },
-  {
-    name: 'Buy cookies',
-    completed: false,
-    id: 4,
-  },
-  {
-    name: 'Look up form page',
-    completed: true,
-    id: 5,
-  },
-  {
-    name: 'Learn angular',
-    completed: true,
-    id: 6,
-  },
-  {
-    name: 'Learn Vue',
-    completed: true,
-    id: 7,
-  },
-  {
-    name: 'Learn React',
-    completed: true,
-    id: 8,
-  },
-  {
-    name: 'Learn Math',
-    completed: true,
-    id: 9,
-  },
-]
-
-const Home = () => {
-  const [taskData, setTaskData] = useState(null);
+const Home = ({ setAlert }) => {
+  const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    setTaskData(fakeApi);
+    const fetchAllProjects = async () => {
+      const fetchedTasks = await taskService.getAllTask();
+      setTasks(fetchedTasks);
+    }
+    fetchAllProjects();
   },[]);
-
-  if(taskData === null) return(<></>);
 
   return(
     <Page>
-      <div className={styles['home-section']}>
+      <div className={'container'}>
+        {/*This displays Project TASKS*/}
         {
-          taskData.map(item =>
-            <Task completed={item.completed}
-              key={item.id}
-              taskId={item.id}
-              taskData={taskData}
-              setTaskData={setTaskData}
+          tasks ?
+          tasks?.map((item, index) =>
+            <Task
+              key={index}
+              completed={item.completed}
+              taskId={item._id}
+              dueDate={item.dueDate}
+              setTasks={setTasks}
+              tasks={tasks}
+              setAlert={setAlert}
             >
-              {item.name}
+              {item.title}
             </Task>
           )
+          : <h1>Empty List</h1>
         }
       </div>
     </Page>
